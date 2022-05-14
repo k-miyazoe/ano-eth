478e49b6224443a2706aa83c90e9c8442f62e170
k-miyazoe <nk.miyazoe@gmail.com>
2022-05-14 23:40:58 +0900

    [update]jwt-backent-done

--- back/app/serializers.py
+++ back/app/serializers.py
@@ -1,12 +1,18 @@
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
-User = get_user_model()
+from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'status')
+        fields = ('id', 'email', 'username', 'password')
         #idを更新不可にできる
         read_only_fields = ('id',)
+        extra_kwargs = {'password': {'required': True}}
+
+
+    def create(self,validated_data):
+        #パスワードをハッシュ化する
+        user = User.objects.create_user(**validated_data)
+        return user
 
